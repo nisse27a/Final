f4156fac69f1758aca5a5853afd61f5608ce4653
nisse27a <71439623+nisse27a@users.noreply.github.com>
2021-04-22 13:27:56 +0200

    Lade till knapparna till mineFlip

--- js/mineFlip.js
+++ js/mineFlip.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let gameTiles = [[],[],[],[],[]];//gameTiles[row][column]
+
+for(let i = 0; i<5; i++) {
+    for(let k = 0; k<5; k++) {
+        let newButton = document.createElement("button");
+        newButton.setAttribute("id","button" + i + "-" + k);
+        newButton.setAttribute("onclick", "tileFlip(\"button" + i + "-" + k + "\")");
+        
+        document.getElementById("gameboard").appendChild(newButton);
+        gameTiles[i][k] = newButton;
+        gameTiles[i][k].innerText = "Tile";
+    }
+}
+
+function tileFlip(buttonId) {
+    console.log(document.getElementById(buttonId));
+}

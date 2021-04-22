8d98d380a91545d3a8007015cd00f8d68cd029f0
nisse27a <71439623+nisse27a@users.noreply.github.com>
2021-04-22 18:58:11 +0200

    lade till funktion för att ge ett numeriskt värde till varje button

--- js/mineFlip.js
+++ js/mineFlip.js
@@ -1,19 +1,37 @@
 "use strict";
 
 let gameTiles = [[],[],[],[],[]];//gameTiles[row][column]
+let gameValues = [[],[],[],[],[]];
 
-for(let i = 0; i<5; i++) {
-    for(let k = 0; k<5; k++) {
+for(let row = 0; row<5; row++) {
+    for(let column = 0; column<5; column++) {
         let newButton = document.createElement("button");
-        newButton.setAttribute("id","button" + i + "-" + k);
-        newButton.setAttribute("onclick", "tileFlip(\"button" + i + "-" + k + "\")");
-        
+        newButton.setAttribute("id","button" + row + "-" + column);
+        newButton.setAttribute("onclick", "tileFlip(\"button" + row + "-" + column + "\")");
+
         document.getElementById("gameboard").appendChild(newButton);
-        gameTiles[i][k] = newButton;
-        gameTiles[i][k].innerText = "Tile";
+        gameTiles[row][column] = newButton;
+        gameTiles[row][column].innerText = "Tile";
     }
 }
 
+function assignValues() {
+    for(let row = 0; row<5;row++) {
+        for(let column = 0; column<5;column++) {
+            gameValues[row][column] = Math.floor(Math.random()*4);
+        }
+    }
+}
+assignValues();
+
 function tileFlip(buttonId) {
     console.log(document.getElementById(buttonId));
+    document.getElementById(buttonId)
+    for(let row = 0; row<5;row++) {
+        for(let column = 0; column<5;column++) {
+            if(("button" + row + "-" + column)==buttonId) {
+                gameTiles[row][column].innerText = gameValues[row][column];
+            }
+        }
+    }
 }

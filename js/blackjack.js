8c15d17a3998a5e6b74e5e80125867253c23a8b9
nisse27a <71439623+nisse27a@users.noreply.github.com>
2021-05-08 18:39:59 +0200

    Påbörjade blackjack sidan, organiserade koden samt började integrera funktionerna med html-koden

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,20 +1,21 @@
 "use strict";
-//#region Betting
 
-//#region dealer
+//#region setup
 
-//#endregion
-
-//#region Player
 let player = {
     Cards: [],
     CardSum: 0,
-    Bet: 0
+    Bet: 0,
+    Money: GetMoney(),
+    PlayField: "player"
 }
-//#endregion
-
-//#region cards
-
+function GetMoney() {
+    let money = localStorage.getItem("bank");
+    if(money=="null"||money==null) {
+        localStorage.setItem("bank","10000")
+    }
+    return localStorage.getItem("bank");
+}
 //Carddeck är organiserad som en nestad array där de nestade arrays är de olika färger
 //[[Diamnods],[Spades],[Hearts],[Clubs]]
 let cardDeck = [[],[],[],[]];
@@ -36,9 +37,9 @@ for(let suit = 0; suit <4;suit++) {
         }
     for(let value = 0; value < 13; value++) {
         let rank;
-        if(value>9||value==1) {
+        if(value>9||value==0) {
             switch(value) {
-                case 1:
+                case 0:
                     rank = "Ace";
                     break;
                 case 10:
@@ -51,6 +52,8 @@ for(let suit = 0; suit <4;suit++) {
                     rank = "King";
                     break;
             }
+        } else {
+            rank = "Basic";
         }
 
         cardDeck[suit][value] = {
@@ -60,10 +63,71 @@ for(let suit = 0; suit <4;suit++) {
         }
     }
 }
+
+function UpdateBank(money) {
+    localStorage.setItem("bank", JSON.stringify(money))
+}   
 //#endregion
 
 //#region CardFunctions
-function PickCard() {
-    
+function PickCard(user) {
+    let suit = Math.floor(Math.random()*4);
+    let value = Math.floor(Math.random()*13);
+    user.Cards.push(cardDeck[suit][value]);
+    PrintCard(user, cardDeck[suit][value]);
+}
+
+function PrintCard(user, card) {
+    if(card.Value>10||card.Value==1) {
+        document.querySelector("." + user.PlayField).innerHTML += "<div>" + card.Rank + "\nof\n" + card.Suit+ "</div>";
+    } else {
+        document.querySelector("." + user.PlayField).innerHTML += "<div>" + card.Value + "\nof\n" + card.Suit+ "</div>";
+    }
 }
 //#endregion
+
+//#region Betting
+document.getElementById("bank").innerText += " " + player.Money;
+
+function ChangeBet(buttonFunction) {
+    let currentBet = document.querySelector("input[type='text']").value;
+    switch(buttonFunction) {
+        case "timesTwo":
+            if(currentBet*2>parseInt(player.Money)) {
+                currentBet = parseInt(player.Money);
+            } else {
+                currentBet *= 2;
+            }
+            break;
+        case "divideTwo":
+            if((currentBet/2)%1==0.5) {
+                currentBet++;
+            }
+            currentBet /= 2;
+            break;
+        case "clear":
+            currentBet = "";
+            break;
+        case "max":
+            currentBet = parseInt(player.Money);
+            break;
+    }
+    document.querySelector("input[type='text']").value = currentBet;
+}
+function Bet() {
+    //lagra bettet i localstorage på nåt sätt
+    player.Bet = document.querySelector("input[type='text']").value;
+    document.getElementById("bet").innerText += " " + player.Bet;
+
+    player.Money = parseInt(player.Money) - parseInt(player.Bet);
+    document.querySelector(".offGame").classList.toggle("invisible");
+    let inGameFields = document.querySelectorAll(".inGame");
+    inGameFields.forEach((field) => {
+        field.classList.toggle("invisible");
+    });
+
+}
+
+//#endregion
+
+
